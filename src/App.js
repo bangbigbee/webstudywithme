2836704607afb43121f6fb30f5cad9e98b18acb7
bangdtbk <bangdtbk@gmail.com>
2023-02-08 11:14:58 +0700

    add analog clock

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import MyForm from './components/MyForm';
+import AnalogClock from './components/AnalogClock';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <MyForm />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <p>
+        <MyForm />
+        <AnalogClock />
+      </p>
+
+
     </div>
   );
 }
--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import MyFormChild from "./MyFormChild";
 class MyForm extends React.Component {
     state = {
         firstName: '',
-        lastName: ''
+        lastName: '',
+        arrJobs: [
+            { id: 'job1', title: 'dev', salary: '500 $' },
+            { id: 'job2', title: 'tester', salary: '300 $' },
+            { id: 'job3', title: 'project manager', salary: '1000 $' }
+        ]
     }
     handleOnChangeFirstName = (event) => {
         this.setState({
@@ -35,6 +40,11 @@ class MyForm extends React.Component {
                     value={this.state.lastName}
                     onChange={(event) => this.handleOnChangeLastName(event)} /> <br />
                 <input type="submit" value="Submit" onClick={(event) => this.handleSubmit(event)} />
+                <MyFormChild
+                    name={this.state.firstName}
+                    age={'25'}
+                    address={'Da Nang'}
+                    arrJobs={this.state.arrJobs} />
             </form>
         )
     }
